57d95d51c8e1d6e7c3226bded128223b4ecbcc11
Sivakumaran M <mskmss1516@gmail.com>
2025-03-06 13:55:27 +0530

    Troubleshooting the pickup page - it overrides the previously added items in the pickup page

--- lib/service/database.dart
+++ lib/service/database.dart
@@ -1,33 +1,35 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:random_string/random_string.dart';
 
 class DatabaseMethods {
   Future addUserDetail(Map<String, dynamic> userInfoMap, String id) async {
-    try {
-      await FirebaseFirestore.instance
-          .collection("users")
-          .doc(id)
-          .set(userInfoMap);
-      print("User data added successfully to Firestore (ID: $id)");
-    } catch (e) {
-      print("Error adding user data to Firestore: $e");
-    }
+    return await FirebaseFirestore.instance
+        .collection("users")
+        .doc(id)
+        .set(userInfoMap);
   }
 
-  // Add Item to Firestore under the user's document and WITH the phone number document
   Future<void> addItem(
       Map<String, dynamic> itemInfo, String phoneNumber, String userId) async {
     // Ensure the phone number is valid
     if (phoneNumber == null || phoneNumber.isEmpty) {
       print("Error: Phone number is null or empty.");
-      return; // Or throw an exception
+      return;
     }
+
+    // Add a unique item ID to the item info
+    String itemId = randomAlphaNumeric(10);
+    itemInfo['itemId'] = itemId;
+
+    // Construct the document path using the user's ID and phone number and Item ID
     try {
-      // Construct the document path using the user's ID and DIRECTLY to the  phone number
       await FirebaseFirestore.instance
           .collection("users") // Top-level collection for all users
           .doc(userId) // Document ID is the user's ID
           .collection("phoneNumbers")
           .doc(phoneNumber)
+          .collection("items")
+          .doc(itemId)
           .set(itemInfo);
       print(
           "Item data added successfully to Firestore (User: $userId, Phone Number: $phoneNumber)");
@@ -39,7 +41,7 @@ class DatabaseMethods {
   Stream<QuerySnapshot> getUploadedItems(String userId) {
     if (userId == null || userId.isEmpty) {
       print("Warning: User ID is null or empty. Returning empty stream.");
-      return const Stream.empty();
+      return const Stream<QuerySnapshot>.empty();
     }
     try {
       return FirebaseFirestore.instance
@@ -49,7 +51,8 @@ class DatabaseMethods {
           .snapshots();
     } catch (e) {
       print("Error getting uploaded items stream: $e");
-      return const Stream.empty(); // Return an empty stream on error
+      return const Stream<
+          QuerySnapshot>.empty(); // Return an empty stream on error
     }
   }
 }
